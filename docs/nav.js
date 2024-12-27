c678cda943584cf0502975c5dfab122a01abfab5
W-E-Tech <it@wattical-evaris-tech.com>
2024-12-27 18:35:10 +0000

    Implements indicator for current nav item

--- docs/nav.js
+++ docs/nav.js
@@ -1,1 +1,11 @@
-document.getElementById('nav').setAttribute('class', window.parent.location.pathname.includes('/start') ? 'dark' : 'light');
+const pageName = window.parent.location.pathname.match(/\/(\w+)\.html$/)[1]
+const navName = pageName.startsWith('solution') ? 'solutions' : pageName;
+
+document
+    .getElementById('nav')
+    .setAttribute('class', pageName === 'start' ? 'dark' : 'light');
+
+document
+    .getElementById(navName)
+    .classList
+    .add('current-nav-item');
