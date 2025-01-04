7ef7aee2ea7027b0cff73b0a40b707a33599e594
W-E-Tech <it@wattical-evaris-tech.com>
2025-01-04 10:38:50 +0000

    Implements footer for all pages

--- src/assets/scripts/render_templates.js
+++ src/assets/scripts/render_templates.js
@@ -1,8 +1,15 @@
-window.onload = function () {
-    fetch('./nav.template.html')
-        .then(function (response) {
-            return response.text();
-        }).then(function (htmlText) {
-            document.querySelector('nav').appendChild(new DOMParser().parseFromString(htmlText, 'text/html').querySelector('template').content.cloneNode(true))
-        });
-}
+const templates = [
+    { path: './nav.template.html', selector: 'nav' },
+    { path: './footer.template.html', selector: 'footer' },
+];
+
+window.addEventListener('load', function () {
+    templates.forEach(function (template) {
+        fetch(template.path)
+            .then(function (response) {
+                return response.text();
+            }).then(function (htmlText) {
+                document.querySelector(template.selector).appendChild(new DOMParser().parseFromString(htmlText, 'text/html').querySelector('template').content.cloneNode(true))
+            });
+    });
+});
